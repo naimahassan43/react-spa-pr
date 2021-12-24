43c55967b2ec10e77de6a73640735aa481d3c00c
Naima Hassan <naima.hassan434@gmail.com>
2021-12-24 15:18:58 +0000

    useHistory hook

--- src/components/TDetails.js
+++ src/components/TDetails.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 const TDetails = () => {
   const params = useParams();
+  let navigate = useNavigate();
+  function handleClick() {
+    navigate("/");
+  }
 
   return (
     <div>
       <h1>Tutorials Details {params.title}</h1>
+      <button onClick={handleClick}>Go Home</button>
+      <button onClick={() => navigate(-2)}>Go 2 pages Back</button>
+      <button onClick={() => navigate(-1)}>Go Back</button>
+      <button onClick={() => navigate(1)}>Go forward</button>
     </div>
   );
 };
